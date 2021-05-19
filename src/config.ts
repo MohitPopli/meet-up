481b2ab81a6b085646b058ea3fbd45e093ac9d31
Mohit <m.popli5@gmail.com>
2021-05-19 09:49:57 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,7 @@
 import firebase from 'firebase';
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBVf2JtrsAVPgY6F2yrNyI2GGScjbjn_eE',
-  authDomain: 'meet-up-853b4.firebaseapp.com',
-  projectId: 'meet-up-853b4',
-  storageBucket: 'meet-up-853b4.appspot.com',
-  messagingSenderId: '57737698542',
-  appId: '1:57737698542:web:65e93f998b977ae88967ff',
-  measurementId: 'G-WH4YJQWCEL',
+  /** Add your config here */
 };
 
 firebase.initializeApp(firebaseConfig);
